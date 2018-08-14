17dc065fec19807d4ec6f0e073bb0fadd6445aae
Craig Reynolds <craig@wyldphyre.com>
2018-08-14 10:42:41 +0800

    Handle opening in an existing instance and when VS Code is installed in the users local app data path

--- VSCodeGoToWrapper/Program.cs
+++ VSCodeGoToWrapper/Program.cs
@@ -12,16 +12,25 @@ namespace VSCodeGoToWrapper
     {
       if (args.Length == 0)
       {
-        MessageBox.Show("VSCodeGoToWrapper.exe <File_Path_To_Open> [Line_Number] [Column_Number] [Path_To_VSCode_Executable]\n\nThe first file path parameter is required.", "Usage");
+        MessageBox.Show("VSCodeGoToWrapper.exe <File_Path_To_Open> [Line_Number] [Column_Number] [UseExisting] [Path_To_VSCode_Executable]\n\nThe first file path parameter is required.", "Usage");
         return;
       }
 
-      const string VSCodePath = @"C:\Program Files\Microsoft VS Code\Code.exe";
+      const string AdminVSCodePath = @"C:\Program Files\Microsoft VS Code\Code.exe";
+      var LocalAppDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+      var UserVSCodePath = Path.Combine(LocalAppDataFolder, @"Programs\Microsoft VS Code\Code.exe");
 
-      var exePath = VSCodePath;
+      var exePath = AdminVSCodePath;
+      if (File.Exists(UserVSCodePath))
+        exePath = UserVSCodePath;
+
+      if (!File.Exists(exePath))
+        MessageBox.Show($"Cannot locate executable for VS Code at: {exePath}");
+
       var line = 1;
       var column = 1;
       var filePath = string.Empty;
+      var useExisting = false;
 
       if (args.Length > 0)
         filePath = args[0];
@@ -39,9 +48,25 @@ namespace VSCodeGoToWrapper
         column = int.Parse(args[2]);
 
       if (args.Length > 3)
-        exePath = args[3];
+      {
+        if (string.Equals(args[3], "UseExisting", StringComparison.CurrentCultureIgnoreCase))
+        {
+          useExisting = true;
 
-      var arguments = $"-n -g {filePath}:{line}:{column}";
+          if (args.Length > 4)
+          {
+            exePath = args[4];
+          }
+        }
+        else
+        {
+          exePath = args[3];
+        }
+      }
+
+      var arguments = $"-g {filePath}:{line}:{column}";
+      if (!useExisting)
+        arguments = "-n " + arguments;
 
       var startInfo = new ProcessStartInfo(exePath, arguments);
       Process.Start(startInfo);
